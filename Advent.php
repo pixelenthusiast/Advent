05572ea8c013ec85f6ae4d16e583292fa1d052da
Semsker <semsker@slub-dresden.de>
2018-12-14 12:44:13 +0100

    first commit

---
+++ Advent.php
@@ -0,0 +1,42 @@
+<?php
+    header('Content-Type: text/plain');
+     $Personen = 9;
+     $Tage = 24;
+     $durch = $Tage / $Personen;
+     $y=1;
+     $x=1;
+     $z=1;
+     $tuer=0;
+
+     while($x <= $Personen){
+        echo $x.". Person:";
+
+        for($y; $y <= $durch; $y++){
+            $tuer = $Personen*($y-1)+$x;
+            echo " ".$tuer;
+        }
+        $y=1;
+        echo "\n";
+        $x=$x+1;
+        //echo $x;
+    }
+    echo "\nJeder hat ".floor($durch)." Türchen geöffnet.\n";
+    echo "\n";
+    if($tuer != $Tage){
+        $rest = $Tage - $tuer;
+
+        echo "Es sind noch ".$rest." Türchen auf die restlichen Personen zu verteilen.\n";
+        echo "\n";
+        $rdmgen = [];
+            for ($y; $y<=$Personen; $y++){
+                $rdmgen[$y]=$y;
+            }
+        shuffle ($rdmgen);
+        echo "Vorschlag: ";
+        for($z; $z<$Personen; $z++){
+            echo " Person ".$rdmgen[$z].",";
+        }
+    }
+
+
+ ?>
